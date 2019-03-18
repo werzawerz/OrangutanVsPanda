ffd7681e555f426b28fedac3efc2aea11a4b4446
Potty <Potty@DESKTOP-RK4G8CH>
2019-03-18 21:52:33 +0100

    dab

--- src/com/company/WeakTile.java
+++ src/com/company/WeakTile.java
@@ -1,4 +1,30 @@
 package com.company;
 
-public class WeakTile {
+/**
+ * Olyan csempe, ami egy bizonyos rálépés után eltörik. Nyilvántartja, hogy mennyi
+ * rálépés van vissza a törésig.
+ */
+public class WeakTile extends Tile {
+
+    /**
+     * A fenmaradó rálépés a törésig.
+     */
+    private int strength;
+
+    /**
+     * @param a : a WeakTilera lépni akaró állat
+     * Magára rakja az Animal-t, ha “élete” 0, akkor pedig
+     * törli azt.
+     */
+    void accept(Animal a) {
+
+    }
+
+    /**
+     * @param i
+     * Csökkenti a strength attributum értékét i-vel
+     */
+    void decreaseStrength(int i) {
+
+    }
 }
