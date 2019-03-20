d68a134b90fe1d37d379201cf493d9a07e5dc967
Márton Herdey <herdeym@gmail.com>
2019-03-20 12:56:19 +0100

    Thing+Tile

--- src/com/company/Thing.java
+++ src/com/company/Thing.java
@@ -1,4 +1,43 @@
 package com.company;
 
-public class Thing {
+/**
+ * A pályáelemek összességének abszrakt ősosztálya.
+ * Nyilvántartja a csempét, amin van.
+ */
+public abstract class Thing {
+
+    /**
+     * Az a Tile, amin aktuálisan van.
+     */
+    private Tile position;
+
+    /**
+     * @param o
+     * @return bool
+     * Akkor hívódik meg, amikor egy leszármazottja találkozik egy Orangutan-nal.
+     * Leszármazottban felül van írva. Itt csak false-szal tér vissza.
+     */
+    public boolean collideWith(Orangutan o){
+
+    }
+
+    /**
+     * @param p
+     * @return bool
+     * Akkor hívódik meg, amikor egy leszármazottja találkozik egy Panda-val.
+     * Leszármazottban felül van írva. Itt csak false-szal tér vissza.
+     */
+    public boolean collideWith(Panda p){
+
+    }
+
+    /**
+     * @return Tile
+     * Visszaadja a dolog pozícióját.
+     */
+    public Tile getPosition(){
+        return position;
+    }
+
+
 }
--- src/com/company/Wardrobe.java
+++ src/com/company/Wardrobe.java
@@ -1,4 +1,35 @@
 package com.company;
 
-public class Wardrobe {
+
+/**
+ * A pálya olyan csempéje, amire rálépve egy másik,
+ * ugyanilyen szekrény típusú csempén jelenik meg az orángután.
+ * Felelőssége a másik szekrény nyilvántartása.
+ */
+public class Wardrobe extends Tile {
+
+    /**
+     * A szomszéd szekrény, melyre át kell teleportálnia a rá lépő állatot.
+     */
+    private Tile neighbourWardrobe;
+
+
+    /**
+     * @param p Panda
+     * p-t átlépteti a szomszédos szekrényre.
+     */
+    public void accept(Panda p){
+        neighbourWardrobe.accept(p);
+
+    }
+
+    /**
+     * @param o Orangutan
+     * o-t átlépteti a szomszédos szekrényre.
+     */
+    public void accept(Orangutan o){
+        neighbourWardrobe.accept(o);
+    }
+
+
 }
