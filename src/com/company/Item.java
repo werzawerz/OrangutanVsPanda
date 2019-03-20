5d08f491f74e05527d8f7009191e415037e44c14
Fógel Péter <fogel.peter@yahoo.com>
2019-03-20 16:35:52 +0200

    FPeti2

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,4 +1,38 @@
 package com.company;
 
-public class Item {
-}
+/**
+ * A nem mozgó pályaelemeket reprezentálja. Absztrakt ősosztálya az Arcade-nek, a
+ * VendingMachine-nek, és a Sofa-nak.
+ */
+public class Item extends Thing{
+
+    /**
+     * @param o
+     * @return bool
+     * Amikor o, egy csempére adott Item-mel,
+     * akkor visszatér false-szal.
+     */
+    public  boolean collidewith(Orangutan o)
+    {
+
+    }
+
+    /**
+     * @param p
+     * @return bool
+     * Amikor p, egy csempére adott Item-mel, akkor
+     * visszatér false-szal.
+     */
+    public  boolean collidewith(Panda p)
+    {
+
+    }
+
+    /**
+     *Értesíti a szomszédos csempéken állókat, ha valami történik,
+     * amiről tudniuk kellene.
+     */
+    public void notify()
+    {
+
+    }
