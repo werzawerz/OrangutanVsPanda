af1b1aa63e14352647eb5c4af91663984677b625
Fógel Péter <fogel.peter@yahoo.com>
2019-03-20 16:20:57 +0200

    FPeti

--- src/com/company/Arcade.java
+++ src/com/company/Arcade.java
@@ -1,4 +1,16 @@
 package com.company;
 
-public class Arcade {
+/**
+ * A játékgépet modellező osztály. Felelőssége a csengetés.
+ */
+public class Arcade extends Item{
+
+    /**
+     * Ez a függvény értesíti a szomszédos csempéken álló
+     * pandákat, hogy csengetés történt.
+     */
+    public void notify()
+    {
+        getPosition().getNeighbours()[0].getAnimal().scare();
+    }
 }
--- src/com/company/Sofa.java
+++ src/com/company/Sofa.java
@@ -1,4 +1,35 @@
 package com.company;
 
-public class Sofa {
+/**
+ * A fotelt reprezántáló osztály, a lusta pandák leülnek rá, ha mellé érnek. Nyilvántartja,
+ * hogy foglalt-e.
+ */
+public class Sofa extends Item{
+
+    /**
+     * A foglaltságot jelzi.
+     */
+    private boolean empty;
+
+    public  boolean collideWith(Panda p){
+        return true;
+    }
+
+    /**
+     * Meghívja a szomszédos csempéken levő állatoknak a sit()
+     * függvényét.
+     */
+    public  void notify(){
+        getPosition().getNeighbours()[0].getAnimal().sit(this);
+
+    }
+
+
+    /**
+     * @return
+     * Visszaadja a fotel foglaltságát jelző bool értékét.
+     */
+    public  boolean isEmpty(){
+
+    }
 }
