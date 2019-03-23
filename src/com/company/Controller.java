6bdf27de2b97772c88cfd8f41c97bd96c6a96c83
Potty <Potty@DESKTOP-RK4G8CH>
2019-03-23 12:16:39 +0100

    Peti 3.23 12:16

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Controller {
+
+    private int tab;
+
+    public int getTab() {
+        return tab;
+    }
+
+    public void incTab() {
+        tab++;
+    }
+
+    public void decTab() {
+        tab--;
+    }
 }
