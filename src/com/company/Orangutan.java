3800d7e795e08275730c1182c39620c4c80471ae
Ricsi <Ricsi@DESKTOP-7U2FN3M>
2019-04-17 20:32:43 +0200

    .

--- src/com/company/Orangutan.java
+++ src/com/company/Orangutan.java
@@ -9,7 +9,15 @@ public class Orangutan extends Animal {
      * A követendő út csempéi.
      */
     private Tile[] path;
+    int timeToSteal;
 
+    int gettimeToSteal(){
+        return timeToSteal;
+    }
+
+    void settimeToSteal(int t) {
+        timeToSteal = t;
+    }
     /**
      * @return int
      * Visszaadja a pandas tömb hosszát.
@@ -22,7 +30,19 @@ public class Orangutan extends Animal {
             return 1 + getPandaNum(p.getNextPanda());
     }
 
-
+    public boolean collideWith(Orangutan o){
+       // String s = this.getClass().getSimpleName().toUpperCase() + ":collideWith(o)";
+        if (o.getNextPanda() == null && this.getNextPanda() != null && o.gettimeToSteal() == 0) {
+            o.setNextPanda(this.getNextPanda());
+            o.getNextPanda().setFollowingA(o);
+            this.settimeToSteal(3);
+            this.setNextPanda(null);
+        }
+      //  Controller.incTab();
+     //   Controller.writeClassAndFunction(s);
+    //    Controller.decTab();
+        return false;
+    }
 
     /**
      * Kiüríti a nextPanda attribútumot.
