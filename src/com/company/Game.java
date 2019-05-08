fcb6facdd1b5d3ffdd93e3d4da26ce2a026b9a5e
Bar987 <osvathbarnabas@gmail.com>
2019-05-08 19:02:37 +0200

    Merge branch 'master' of /home/bar/IdeaProjects/OrangutanVsPanda with conflicts.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,19 +1,26 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Game extends JFrame {
-    JPanel gamePanel ;
+    JPanel gamePanel;
+    JPanel menuPanel;
     static private int points;
     public Game() {
         super();
         
         gamePanel = new JPanel();
+        menuPanel = menu();
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-        setContentPane(gamePanel);
-
+        //setContentPane(gamePanel);
+        //add(gamePanel);
+        getContentPane().add(menuPanel);
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -46,5 +53,23 @@ public class Game extends JFrame {
      //       t.draw(this.getGraphics());
      //   }
 
+    }
+    JPanel menu(){
+        JPanel menu = new JPanel();
+        try {
+            JButton play = new JButton("Play");
+            JButton exit = new JButton("Exit");
+            BufferedImage myPic = ImageIO.read(new File("images/menu.png"));
+            JLabel picLabel = new JLabel(new ImageIcon(myPic));
+
+            play.setBounds(100, 500, 200, 40);
+            exit.setBounds(500, 500, 200, 40);
+            menu.setLayout(null);
+            //menu.add(picLabel);
+            menu.add(play);
+            menu.add(exit);
+        }
+        catch(IOException e){}
+        return menu;
     }
 }
